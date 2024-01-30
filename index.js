fc55009e3fa28a9a5cecd1bf7f4c5ebfb817e1e3
Fazifazz <fasilpambalath7@gmail.com>
2024-01-30 23:55:06 +0530

    app.use public

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 
 app.set('view engine','ejs')
 app.use(express.static('public'));
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(express.urlencoded({extended:true}));
 app.use(session({
